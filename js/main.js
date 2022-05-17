13dc8ceb08a0e02794b0a3b3fff3943ab383303d
InhaKr <inkr3891@gmail.com>
2022-05-17 11:23:00 +0300

    ДЗ 13. #16_2

--- js/main.js
+++ js/main.js
@@ -1,25 +1,60 @@
-"use strict";
+// "use strict";
 
-//д.з 5 #7_2
-function more(a, b) {
+// ДЗ 13. #16_2
+// Написать скрипт который в title
+// страницы(document.title) будет
+// выводить текущее время и количество
+// секунд которое прошло после открытия
+// страниџы.
+// Сгенерировать при помощи JS элемент
+//  div с двумя span, где выводится 
+//  тоже самое время, что и в title 
 
-  let d = 0;
-  let r = 0;
+//  в разные span соответственно
 
-  for (let i = 0; i < a.length; i++) {
-    // console.log(a[i]);
-    d += a[i];
-  }
 
-  for (let j = 0; j < a.length; j++) {
-    // console.log(a[i]);
-    r += b[j];
-  }
 
-  if (d > r) {
-    return (a);
-  } else {
-    return (b);
-  }
-}
-console.log(more([0,2], [-8,1]));
+let l = setInterval(function clock() {
+  let d = new Date();
+
+  let hours = d.getHours();
+  let minutes = d.getMinutes();
+  let seconds = d.getSeconds();
+  let time = performance.now();
+
+  document.querySelector(".doc_time").innerHTML = (hours < 10 ? '0' : '') + hours + ":" + (minutes < 10 ? '0' : '') + minutes +  ":" + (seconds < 10 ? '0' : '') + seconds +'   //   '+ time;
+  document.querySelector(".span1").innerHTML = (hours < 10 ? '0' : '') + hours + ":" + (minutes < 10 ? '0' : '') + minutes +  ":" + (seconds < 10 ? '0' : '') + seconds +'   //   '+ time;
+  document.querySelector(".span2").innerHTML = (hours < 10 ? '0' : '') + hours + ":" + (minutes < 10 ? '0' : '') + minutes +  ":" + (seconds < 10 ? '0' : '') + seconds +'   //   '+ time;
+},1000);
+
+
+
+let x = document.createElement('div');
+x.classList.add('myDiv');
+x.innerHTML = 'myDiv';
+x.style.backgroundColor= 'blue';
+ 
+
+let container = document.querySelector('.container');
+container.appendChild(x);
+
+let y = document.createElement('span');
+y.classList.add('span1');
+y.innerHTML = 'time';
+y.style.backgroundColor= 'yellow';
+ 
+
+let r = document.querySelector('.myDiv');
+r.appendChild(y);
+
+let z = document.createElement('span');
+z.classList.add('span2');
+z.innerHTML = l;
+z.style.backgroundColor= 'orange';
+ 
+
+let s = document.querySelector('.myDiv');
+s.appendChild(z);
+
+
+
